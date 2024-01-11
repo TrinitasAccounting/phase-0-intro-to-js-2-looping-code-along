f16fabd0ccde4903c74a132166fbe48f70909955
Clay Mangum <clay@trinitasaccounting.com>
2024-01-11 16:23:05 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+
+function writeCards(array, event) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+};
+
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    };
+}
+
+
+
+
+
+
+
+
+
